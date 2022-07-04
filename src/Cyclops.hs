2d7bf534e227462d7c896a6664d70adfe1fd5e5c
Noah Luck Easterly <noah.easterly@gmail.com>
2022-07-03 20:58:39 -0400

    FIx getOps signature to line up better with documentation

--- src/Cyclops.hs
+++ src/Cyclops.hs
@@ -2,6 +2,7 @@
     BlockArguments
   , FlexibleContexts
   , LambdaCase
+  , NamedFieldPuns
 #-}
 module Cyclops
   ( getOps
@@ -16,8 +17,8 @@ import System.IO (hPutStrLn, stderr)
 import Cyclops.Internal hiding (Ops(..), FromArgument(..), sym, app_prec)
 import Cyclops.Internal (Ops, FromArgument)
 
-getOps :: Ops IO t => Version -> Description -> IO t
-getOps version description = do
+getOps :: Ops IO t => Conf -> IO t
+getOps Conf{version,description} = do
   progName <- ProgName <$> getProgName
   getArgs >>= getOpsFrom version description progName >>= flip either pure \case
     ExitWith exit msg -> do
@@ -28,3 +29,8 @@ getOps version description = do
     Completion mmsg -> do
       putStr =<< mmsg
       exitSuccess
+
+data Conf = Conf
+  { version :: Version
+  , description :: Description
+  }
